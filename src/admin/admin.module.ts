f8f8ff540443dcf5384fedc31c4cc359be662709
Dohyun Kim <76870940+dohyun06@users.noreply.github.com>
2025-12-27 04:13:19 +0900

    feat: admin login api

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from '@lib/prisma';
+import { InfoteamIdpModule } from '@lib/infoteam-idp';
+import { JwtModule } from '@nestjs/jwt';
+import ms, { StringValue } from 'ms';
 
 @Module({
+  imports: [
+    ConfigModule,
+    PrismaModule,
+    InfoteamIdpModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn:
+            ms(configService.getOrThrow<StringValue>('JWT_EXPIRE')) / 1000,
+          algorithm: 'HS256',
+          audience: configService.get<string>('JWT_AUDIENCE'),
+          issuer: configService.get<string>('JWT_ISSUER'),
+        },
+      }),
+    }),
+  ],
   controllers: [AdminController],
   providers: [AdminService],
 })
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,26 @@
+import { InfoteamIdpService } from '@lib/infoteam-idp';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { AdminRepository } from './admin.repository';
+import { JwtToken } from './dto/jwtToken.dto';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly infoteamIdpService: InfoteamIdpService,
+    private readonly adminRepository: AdminRepository,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(auth: string): Promise<JwtToken> {
+    const idpToken = auth.split(' ')[1];
+    const userinfo = await this.infoteamIdpService.getUserInfo(idpToken);
+    await this.adminRepository.findAdmin(userinfo.uuid);
+    const tokens = {
+      access_token: this.jwtService.sign({}, { subject: userinfo.uuid }),
+    };
+    return { ...tokens };
+  }
+}
