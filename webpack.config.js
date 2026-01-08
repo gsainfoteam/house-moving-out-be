4fdce2134ad7d794cc9d132f13c41eae83226f4c
siwonpada <psw041115@gmail.com>
2026-01-08 21:35:52 +0900

    Use: webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// webpack.config.js
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = function (options) {
+  return {
+    ...options,
+    entry: options.entry,
+    externals: [nodeExternals()],
+    resolve: {
+      ...options.resolve,
+      // 중요: .js 확장자로 호출하더라도 .ts 파일을 먼저 찾도록 매핑
+      extensionAlias: {
+        '.js': ['.ts', '.js'],
+      },
+    },
+  };
+};
