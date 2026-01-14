3106bc49de3a0c6f9bbd88b1cff88995d8a103db
siwonpada <psw041115@gmail.com>
2026-01-14 19:23:08 +0900

    fix: webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ module.exports = function (options) {
   return {
     ...options,
     entry: options.entry,
-    externals: [nodeExternals()],
+    externals: [nodeExternals({
+      allowlist: [/^file-type/],
+    })],
     resolve: {
       ...options.resolve,
       // 중요: .js 확장자로 호출하더라도 .ts 파일을 먼저 찾도록 매핑
