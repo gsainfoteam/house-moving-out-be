2793d77b690a46287a4856f8001da00bffad996b
Dohyun Kim <76870940+dohyun06@users.noreply.github.com>
2026-01-20 23:14:09 +0900

    refactor: simplify response serialization

--- src/inspector/dto/res/inspector-res.dto.ts
+++ src/inspector/dto/res/inspector-res.dto.ts
@@ -1,38 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Exclude, Expose, Transform } from 'class-transformer';
+import { Transform } from 'class-transformer';
 import { InspectionSlot } from 'generated/prisma/client';
+import { InspectorWithSlots } from 'src/inspector/types/inspector-with-slots.type';
 import { InspectionSlotResDto } from 'src/move-out/dto/res/move-out-schedule-with-slots-res.dto';
 
 export class InspectorResDto {
-  @Expose()
   @ApiProperty({
     example: '550e8400-e29b-41d4-a716-446655440000',
     description: 'Inspector UUID',
   })
   uuid: string;
 
-  @Expose()
   @ApiProperty({
     example: 'John Doe',
     description: 'Inspector name',
   })
   name: string;
 
-  @Expose()
   @ApiProperty({
     example: 'email@gm.gist.ac.kr',
     description: 'Inspector email',
   })
   email: string;
 
-  @Expose()
   @ApiProperty({
     example: '20250000',
     description: 'Inspector student number',
   })
   studentNumber: string;
 
-  @Expose()
   @ApiProperty({
     description: 'Available inspection times',
     type: [InspectionSlotResDto],
@@ -40,12 +36,9 @@ export class InspectorResDto {
   @Transform(({ value }: { value: { inspectionSlot: InspectionSlot }[] }) =>
     value.map((slot) => slot.inspectionSlot),
   )
-  availableSlots: InspectionSlotResDto[] | { inspectionSlot: InspectionSlot }[];
+  availableSlots: InspectionSlotResDto[];
 
-  @Exclude()
-  availableSlotIds: string[];
-
-  constructor(partial: Partial<InspectorResDto>) {
+  constructor(partial: InspectorWithSlots) {
     Object.assign(this, partial);
   }
 }
