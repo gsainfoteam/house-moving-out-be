e7c8439b5ccc10f8e3e3852a401aa34912799d6d
2paperstar <lee@paperst.ar>
2026-01-27 09:15:29 +0900

    chore: merge migration

---
+++ prisma/migrations/20260127001429_admin_to_user/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the `admin_refresh_token` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "admin_refresh_token" DROP CONSTRAINT "admin_refresh_token_admin_uuid_fkey";
+
+-- DropTable
+DROP TABLE "admin_refresh_token";
+
+-- AddForeignKey
+ALTER TABLE "admin" ADD CONSTRAINT "admin_uuid_fkey" FOREIGN KEY ("uuid") REFERENCES "user"("uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `admin` table. All the data in the column will be lost.
+  - You are about to drop the column `phone_number` on the `admin` table. All the data in the column will be lost.
+  - You are about to drop the column `student_number` on the `admin` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "admin" DROP CONSTRAINT "admin_uuid_fkey";
+
+-- AlterTable
+ALTER TABLE "admin" DROP COLUMN "name",
+DROP COLUMN "phone_number",
+DROP COLUMN "student_number";
