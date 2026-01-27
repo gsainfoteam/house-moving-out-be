4cdffcd25fc8c14b4befd42c67f8ca71bdf04918
2paperstar <lee@paperst.ar>
2026-01-27 09:33:45 +0900

    feat: remove `/user` prefix in auth endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -37,6 +37,7 @@ export class AuthController {
     summary: 'User Login',
     description:
       'Issue JWT token. If consent required, returns 403 with CONSENT_REQUIRED or CONSENT_UPDATE_REQUIRED error.',
+    deprecated: true,
   })
   @ApiBody({
     type: UserLoginDto,
@@ -78,6 +79,7 @@ export class AuthController {
   @ApiOperation({
     summary: 'Refresh token',
     description: 'Refresh the access token for user',
+    deprecated: true,
   })
   @ApiCreatedResponse({ type: JwtToken, description: 'Return jwt token' })
   @ApiUnauthorizedResponse({ description: 'Unauthorized' })
@@ -107,6 +109,7 @@ export class AuthController {
   @ApiOperation({
     summary: 'Logout',
     description: 'Logout the user from the cookie. Delete the refresh token.',
+    deprecated: true,
   })
   @ApiOkResponse({ description: 'Logout' })
   @ApiUnauthorizedResponse({ description: 'Unauthorized' })
@@ -116,6 +119,98 @@ export class AuthController {
   @Post('user/logout')
   @UseGuards(UserGuard)
   async userLogout(
+    @GetUser() user: User,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<void> {
+    await this.authService.userLogout(user.uuid);
+    res.clearCookie('user_refresh_token', {
+      path: '/auth',
+    });
+  }
+
+  @ApiOperation({
+    summary: 'User Login',
+    description:
+      'Issue JWT token. If consent required, returns 403 with CONSENT_REQUIRED or CONSENT_UPDATE_REQUIRED error.',
+  })
+  @ApiBody({
+    type: UserLoginDto,
+    required: false,
+    description: 'Consent information (required only when consent needed)',
+  })
+  @ApiOkResponse({ type: JwtToken, description: 'Login success' })
+  @ApiUnauthorizedResponse({ description: 'Invalid IDP token' })
+  @ApiResponse({
+    status: 403,
+    description: 'Consent required',
+    type: ConsentRequiredErrorDto,
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @ApiOAuth2(['email', 'profile', 'student_id', 'phone_number'], 'oauth2')
+  @Post('login')
+  async login(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+    @Body() body?: UserLoginDto,
+  ): Promise<JwtToken> {
+    const auth = req.headers['authorization'];
+    if (!auth) throw new UnauthorizedException();
+
+    const { access_token, refresh_token, refreshTokenExpiredAt } =
+      await this.authService.userLogin(auth, body);
+
+    res.cookie('user_refresh_token', refresh_token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      expires: refreshTokenExpiredAt,
+      path: '/auth',
+    });
+
+    return { access_token };
+  }
+
+  @ApiOperation({
+    summary: 'Refresh token',
+    description: 'Refresh the access token for user',
+  })
+  @ApiCreatedResponse({ type: JwtToken, description: 'Return jwt token' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @Post('refresh')
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<JwtToken> {
+    const refreshToken = req.cookies['user_refresh_token'] as string;
+    if (!refreshToken) throw new UnauthorizedException();
+
+    const result: IssueTokenType =
+      await this.authService.userRefresh(refreshToken);
+    const { access_token, refresh_token, refreshTokenExpiredAt } = result;
+    res.cookie('user_refresh_token', refresh_token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      expires: refreshTokenExpiredAt,
+      path: '/auth',
+    });
+
+    return { access_token };
+  }
+
+  @ApiOperation({
+    summary: 'Logout',
+    description: 'Logout the user from the cookie. Delete the refresh token.',
+  })
+  @ApiOkResponse({ description: 'Logout' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @ApiBearerAuth('user')
+  @Post('logout')
+  @UseGuards(UserGuard)
+  async logout(
     @GetUser() user: User,
     @Res({ passthrough: true }) res: Response,
   ): Promise<void> {
