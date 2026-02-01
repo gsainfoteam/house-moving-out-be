c908d4a5c55e90e76978707e39fc28bf909b6399
2paperstar <lee@paperst.ar>
2026-02-01 12:17:36 +0900

    fix: wrong migration

--- prisma/migrations/20260201030927_add_status_on_schedule/migration.sql
+++ prisma/migrations/20260201030927_add_status_on_schedule/migration.sql
@@ -4,4 +4,4 @@ CREATE TYPE "ScheduleStatus" AS ENUM ('DRAFT', 'ACTIVE', 'COMPLETED');
 -- AlterTable
 ALTER TABLE "move_out_schedule" ADD COLUMN     "status" "ScheduleStatus" DEFAULT 'DRAFT';
 UPDATE "move_out_schedule" SET "status" = 'DRAFT';
-ALTER TABLE "move_out_schedule" ALTER COLUMN   "status" "ScheduleStatus" NOT NULL DEFAULT 'DRAFT';
+ALTER TABLE "move_out_schedule" ALTER COLUMN   "status" SET NOT NULL;
