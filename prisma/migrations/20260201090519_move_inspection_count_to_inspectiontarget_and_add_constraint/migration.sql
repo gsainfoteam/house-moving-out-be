51c471912b2e0a9796daf3f32d13126028f573cf
tkjftkjf <qkrwlsdn1234321@gmail.com>
2026-02-01 18:07:30 +0900

    chore: integrate migration file

--- prisma/migrations/20260201090519_move_inspection_count_to_inspectiontarget_and_add_constraint/migration.sql
+++ prisma/migrations/20260201090519_move_inspection_count_to_inspectiontarget_and_add_constraint/migration.sql
@@ -1,6 +1,7 @@
 /*
   Warnings:
 
+  - You are about to drop the column `re_inspection_count` on the `inspection_application` table. All the data in the column will be lost.
   - The `status` column on the `move_out_schedule` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - The `role` column on the `user` table would be dropped and recreated. This will lead to data loss if there is data in the column.
 
@@ -10,6 +11,12 @@ CREATE TYPE "schedule_status" AS ENUM ('DRAFT', 'ACTIVE', 'COMPLETED', 'CANCELED
 
 -- CreateEnum
 CREATE TYPE "role" AS ENUM ('ADMIN', 'USER');
+
+-- AlterTable
+ALTER TABLE "inspection_application" DROP COLUMN "re_inspection_count";
+
+-- AlterTable
+ALTER TABLE "inspection_target" ADD COLUMN     "inspection_count" INTEGER NOT NULL DEFAULT 0;
 
 -- AlterTable
 ALTER TABLE "move_out_schedule" DROP COLUMN "status",
@@ -24,3 +31,11 @@ DROP TYPE "Role";
 
 -- DropEnum
 DROP TYPE "ScheduleStatus";
+
+-- AlterTable
+ALTER TABLE "inspection_slot"
+ADD CONSTRAINT "InspectionSlot_maleReservedCount_check" CHECK ("male_reserved_count" >= 0),
+ADD CONSTRAINT "InspectionSlot_femaleReservedCount_check" CHECK ("female_reserved_count" >= 0);
+
+-- AlterTable
+ALTER TABLE "inspection_target" ADD CONSTRAINT "InspectionTargetInfo_inspectionCount_check" CHECK ("inspection_count" >= 0);
