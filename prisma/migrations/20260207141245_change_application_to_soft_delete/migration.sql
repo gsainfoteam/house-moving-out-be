1681e6eca3d52d7d9873b476efe599543337ad57
tkjftkjf <qkrwlsdn1234321@gmail.com>
2026-02-09 03:06:40 +0900

    feat: delete updated_at on application schema

--- prisma/migrations/20260207141245_change_application_to_soft_delete/migration.sql
+++ prisma/migrations/20260207141245_change_application_to_soft_delete/migration.sql
@@ -8,6 +8,9 @@
 DROP INDEX "inspection_application_inspection_target_info_uuid_key";
 
 -- AlterTable
-ALTER TABLE "inspection_application" ADD COLUMN     "deleted_at" TIMESTAMP(3);
+ALTER TABLE "inspection_application" ADD COLUMN     "deleted_at" TIMESTAMP(3),
+DROP COLUMN "updated_at";
 
 CREATE UNIQUE INDEX "unique_active_application_per_target" ON "inspection_application"("inspection_target_info_uuid") WHERE "deleted_at" IS NULL;
+
+
