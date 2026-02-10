03c66a5fb84ddee5e3c924c33e148001651811d2
tkjftkjf <qkrwlsdn1234321@gmail.com>
2026-02-10 22:44:51 +0900

    feat: remove unique constraint on application

--- prisma/migrations/20260207141245_change_application_to_soft_delete/migration.sql
+++ prisma/migrations/20260207141245_change_application_to_soft_delete/migration.sql
@@ -10,6 +10,4 @@ DROP INDEX "inspection_application_inspection_target_info_uuid_key";
 -- AlterTable
 ALTER TABLE "inspection_application" ADD COLUMN     "deleted_at" TIMESTAMP(3);
 
-CREATE UNIQUE INDEX "unique_active_application_per_target" ON "inspection_application"("inspection_target_info_uuid") WHERE "deleted_at" IS NULL;
-
 
