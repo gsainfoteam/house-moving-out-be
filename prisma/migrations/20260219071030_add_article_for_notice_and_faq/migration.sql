26eaf4cd4516c57c9cca3c1dafb816ad00c161c8
tkjftkjf <qkrwlsdn1234321@gmail.com>
2026-02-19 16:21:06 +0900

    feat: add Article model and update ArticleType enum in schema

---
+++ prisma/migrations/20260219071030_add_article_for_notice_and_faq/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "article_type" AS ENUM ('NOTICE', 'FAQ');
+
+-- CreateTable
+CREATE TABLE "Article" (
+    "uuid" TEXT NOT NULL,
+    "type" "article_type" NOT NULL,
+    "title_ko" TEXT NOT NULL,
+    "title_en" TEXT NOT NULL,
+    "content_ko" TEXT NOT NULL,
+    "content_en" TEXT NOT NULL,
+    "is_visible" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("uuid")
+);
