a7a82d88a65641fcea4ceeab2cde50d0069b010d
tkjftkjf <qkrwlsdn1234321@gmail.com>
2026-02-19 16:45:36 +0900

    feat: add dtos for craeting article

---
+++ src/article/dto/article.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsString } from 'class-validator';
+
+export enum Language {
+  KO,
+  EN,
+}
+export class ArticleDto {
+  @ApiProperty({
+    description: 'Language of Article',
+    example: Language.KO,
+    enum: Language,
+  })
+  @IsEnum(Language)
+  language: Language;
+
+  @ApiProperty({
+    description: 'Title of Article',
+    example: '일정 변경 공지',
+  })
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    description: 'Content of Article',
+    example: '일정을 하루 앞당깁니다.',
+  })
+  @IsString()
+  content: string;
+}
---
+++ src/article/dto/req/create-article-req.dto.ts
@@ -0,0 +1,55 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ArticleDto } from '../article.dto';
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  ValidateNested,
+} from 'class-validator';
+import { ArticleType } from 'generated/prisma/client';
+import { Type } from 'class-transformer';
+
+export class createArticleReqDto {
+  @ApiProperty({
+    description: 'Type of Article(Notice,Faq)',
+    example: ArticleType.NOTICE,
+    enum: ArticleType,
+  })
+  @IsEnum(ArticleType)
+  type: ArticleType;
+
+  @ApiProperty({
+    description: 'Articles in Korean and English',
+    example: [
+      {
+        language: 'KO',
+        title: '일정 변경 공지',
+        content: '일정을 하루 앞당깁니다.',
+      },
+      {
+        language: 'EN',
+        title: 'Notice for schedule change',
+        content: 'We are moving the schedule up by one day.',
+      },
+    ],
+    type: [ArticleDto],
+    maxItems: 2,
+    minItems: 2,
+  })
+  @IsArray()
+  @ArrayMaxSize(2)
+  @ArrayMinSize(2)
+  @ValidateNested({ each: true })
+  @Type(() => ArticleDto)
+  articles: ArticleDto[];
+
+  @ApiProperty({
+    description: 'Visibility of Article',
+    example: true,
+  })
+  @IsBoolean()
+  @Type(() => Boolean)
+  isVisible: boolean;
+}
