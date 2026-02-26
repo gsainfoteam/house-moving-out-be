4479ac6effae7475b2bddd6ff778718a37c9877d
2paperstar <lee@paperst.ar>
2026-02-26 15:24:34 +0900

    fix: wrong migration

--- prisma/migrations/20260226061243_add_inspection_count_on_application/migration.sql
+++ prisma/migrations/20260226061243_add_inspection_count_on_application/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE "inspection_application" ADD COLUMN     "inspection_count" INTEGER;
 
 -- Update existing rows with 0
-UPDATE "inspection_application" SET "inspection_count" = 0;
+UPDATE "inspection_application" SET "inspection_count" = 1;
 
 -- Make column NOT NULL
 ALTER TABLE "inspection_application" ALTER COLUMN "inspection_count" SET NOT NULL;
