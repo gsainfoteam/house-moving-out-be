fcbd20599825031a269822b1bb09872594390257
2paperstar <lee@paperst.ar>
2026-03-21 10:01:12 +0900

    chore(cd): more esm

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = function (options) {
     entry: options.entry,
     externals: [
       nodeExternals({
-        allowlist: [/^file-type/, /^token-types/],
+        allowlist: [/^file-type/, /^token-types/, /^@borewit/],
       }),
     ],
     resolve: {
