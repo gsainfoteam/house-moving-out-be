2c160bd05f6ec5ffb9e6ab4778a778842f62bc12
2paperstar <lee@paperst.ar>
2026-03-21 10:15:54 +0900

    chore(cd): node externals with scanning node_module

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 // webpack.config.js
 const nodeExternals = require('webpack-node-externals');
+const fs = require('node:fs');
+
+const modules = fs.readdirSync('./node_modules');
+const packages = modules
+  .filter((module) => fs.existsSync(`./node_modules/${module}/package.json`))
+  .map((module) => {
+    return JSON.parse(
+      fs.readFileSync(`./node_modules/${module}/package.json`, 'utf8'),
+    );
+  });
+const allowlist = packages
+  .filter((packageJson) => packageJson.type === 'module')
+  .map((packageJson) => packageJson.name)
+  .map((name) => new RegExp(`^${name}`));
 
 module.exports = function (options) {
   return {
     ...options,
     entry: options.entry,
-    externals: [
-      nodeExternals({
-        allowlist: [
-          /^file-type/,
-          /^token-types/,
-          /^@borewit/,
-          /^strtok3/,
-          /^@tokenizer/,
-        ],
-      }),
-    ],
+    externals: [nodeExternals({ allowlist })],
     resolve: {
       ...options.resolve,
       // 중요: .js 확장자로 호출하더라도 .ts 파일을 먼저 찾도록 매핑
