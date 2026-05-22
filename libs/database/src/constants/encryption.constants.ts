93c4af5a7a5a78d56632b3d6f736056f0664944e
Dohyun Kim <dohyun06b@gmail.com>
2026-05-22 10:06:52 +0900

    refactor: restructure encryption purpose constants

--- libs/database/src/constants/encryption.constants.ts
+++ libs/database/src/constants/encryption.constants.ts
@@ -1,26 +1,28 @@
-export const ENCRYPTION_PURPOSE = {
-  USER: {
-    NAME: 'user:name',
-    EMAIL: 'user:email',
-    PHONE_NUMBER: 'user:phoneNumber',
-    STUDENT_NUMBER: 'user:studentNumber',
-  },
-  INSPECTOR: {
-    NAME: 'inspector:name',
-    EMAIL: 'inspector:email',
-    STUDENT_NUMBER: 'inspector:studentNumber',
-  },
-  TARGET: {
-    STUDENT1_NAME: 'target:student1:name',
-    STUDENT1_STUDENT_NUMBER: 'target:student1:studentNumber',
-    STUDENT2_NAME: 'target:student2:name',
-    STUDENT2_STUDENT_NUMBER: 'target:student2:studentNumber',
-    STUDENT3_NAME: 'target:student3:name',
-    STUDENT3_STUDENT_NUMBER: 'target:student3:studentNumber',
-  },
+const USER = {
+  NAME: 'user:name',
+  EMAIL: 'user:email',
+  PHONE_NUMBER: 'user:phoneNumber',
+  STUDENT_NUMBER: 'user:studentNumber',
 } as const;
 
+const INSPECTOR = {
+  NAME: 'inspector:name',
+  EMAIL: 'inspector:email',
+  STUDENT_NUMBER: 'inspector:studentNumber',
+} as const;
+
+const TARGET = {
+  STUDENT1_NAME: 'target:student1:name',
+  STUDENT1_STUDENT_NUMBER: 'target:student1:studentNumber',
+  STUDENT2_NAME: 'target:student2:name',
+  STUDENT2_STUDENT_NUMBER: 'target:student2:studentNumber',
+  STUDENT3_NAME: 'target:student3:name',
+  STUDENT3_STUDENT_NUMBER: 'target:student3:studentNumber',
+} as const;
+
+export const ENCRYPTION_PURPOSE = { USER, INSPECTOR, TARGET } as const;
+
 export type EncryptionPurpose =
-  | (typeof ENCRYPTION_PURPOSE.USER)[keyof typeof ENCRYPTION_PURPOSE.USER]
-  | (typeof ENCRYPTION_PURPOSE.INSPECTOR)[keyof typeof ENCRYPTION_PURPOSE.INSPECTOR]
-  | (typeof ENCRYPTION_PURPOSE.TARGET)[keyof typeof ENCRYPTION_PURPOSE.TARGET];
+  | (typeof USER)[keyof typeof USER]
+  | (typeof INSPECTOR)[keyof typeof INSPECTOR]
+  | (typeof TARGET)[keyof typeof TARGET];
